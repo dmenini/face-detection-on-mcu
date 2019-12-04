562ffac4760e4cef50ae3122c42273cc72d318a2
dmenini <dmenini@student.ethz.ch>
2019-12-04 02:10:31 +0100

    h5, height filter

--- face_detect.py
+++ face_detect.py
@@ -2,37 +2,58 @@ import lib
 from mtcnn.mtcnn import MTCNN
 import cv2
 import matplotlib.pyplot as plt
-
+import pandas as pd
 
 
 def main():
     # =====================================================================
-    #                      LOAD AND ANALYZE DATA
+    #                      LOAD AND STORE DATA
     # =====================================================================
 
-    train_dict = lib.load2dict(dataset='train', save=False)
-    val_dict = lib.load2dict(dataset='val', save=False)
+    # DO ONLY FIRST TIME!!
+    # train_dict = lib.initialize(dataset='train')
+    # val_dict = lib.initialize(dataset='val')
+
+    train_dict = lib.pickle_load('input/train_dict.pickle')
+    val_dict = lib.pickle_load('input/val_dict.pickle')
+    print("Total train images: ", len(train_dict))
+    print("Total val images: ", len(val_dict))
+
+    # EXAMPLE: HOW TO ACCESS AN IMAGE IN h5 AND PLOT IT WITH BOXES
+    # image = lib.hdf5_load(train_dict[1]['h5_path'])
+    # lib.plot_box(image, train_dict[1]['labels'])
+
+    # To plot the boxes on the image from the dicts
     # lib.plot_box_from_dict(train_dict, max_iter=10, event='all')
+
+    train_heights = lib.heights_description(train_dict)
+    val_heights = lib.heights_description(val_dict)
 
     # =====================================================================
     #                           PREPROCESSING
     # =====================================================================
-    train_dict = lib.filter_small_bbx(train_dict, 200, debug=False)
-    # lib.plot_box_from_dict(train_dict, max_iter=10, event='all')
 
+    h_cut = int(pd.Series(train_heights).quantile(q=0.95))
+    train_dict = [img for img in train_dict if img['shape'][0] <= h_cut]
+    val_dict = [img for img in val_dict if img['shape'][0] <= h_cut]
 
+    train_dict = lib.filter_box(train_dict, 200, debug=False)
+    lib.plot_box_from_dict(train_dict, max_iter=5, event='all')
 
     # =====================================================================
     #                             DETECTION
     # =====================================================================
 
+    print("Final train images: ", len(train_dict))
+    print("Final val images: ", len(val_dict))
+
     detector = MTCNN()
-    for i in range(10):
-        image = cv2.imread(val_dict[i]['path'])
+    for i in range(3):
+        image = lib.hdf5_load(val_dict[i]['h5_path'])
         result = detector.detect_faces(image)
         expected = val_dict[i]['labels']
-        lib.plot_box(image, result, color='r', debug=True, show=False)
-        lib.plot_box(image, expected, color='g', debug=True)
+        lib.plot_box(image, result, color='g', debug=False, show=False)
+        lib.plot_box(image, expected, color='r', debug=False)
 
 
 if __name__ == "__main__":
--- lib.py
+++ lib.py
@@ -1,12 +1,13 @@
-import pickle
-from scipy.io import loadmat
-from itertools import chain
-
-from PIL import Image
+import os
+import cv2
 import numpy as np
-import matplotlib.image as mpimg
+import h5py
+import pickle
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+from scipy.io import loadmat
+from itertools import chain
 
 
 labels_keys = (['__header__', '__version__', '__globals__',  # Attributes
@@ -21,14 +22,38 @@ labels_keys = (['__header__', '__version__', '__globals__',  # Attributes
                 'pose_label_list'                               # typical pose->0, atypical pose->1
                 ])
 
+# ======================================================================================================================
+#                                               DATASET MANAGEMENT
+# ======================================================================================================================
 
-def load2dict(dataset, save=False):
-    """
-    Load input dataset to dictionary
-    :param dataset:
-    :param save: whether to save the dictionary to pickle file
-    :return: list of dictionaries --> keys: int = 'id', str = 'path', int = 'n_faces', list of dictionaries = 'labels'{'box, 'confidence'}
-    """
+def hdf5_store(image, path):
+    # Create a new HDF5 file
+    f = h5py.File(path, "w")
+    # Create a dataset in the file
+    data_set = f.create_dataset("image", np.shape(image), h5py.h5t.STD_U8BE, data=image)
+    f.close()
+
+
+def hdf5_load(path):
+    with h5py.File(path, 'r') as f:
+        # print("Keys in h5 dataset: %s" % f.keys())
+        image = f['image']
+        image = np.array(image[:,:,:])
+    return image
+
+
+def pickle_store(obj, path):
+    f = open(path, 'wb')
+    pickle.dump(obj, f)
+    f.close()
+
+
+def pickle_load(path):
+    f = open(path, 'rb')
+    return pickle.load(f)
+
+
+def initialize(dataset):
     print("Generating " + str(dataset) + " dictionary...")
     face_split = loadmat('input/wider_face_split/wider_face_' + str(dataset) + '.mat')
     event_list = list(chain.from_iterable(face_split['event_list']))
@@ -42,22 +67,31 @@ def load2dict(dataset, save=False):
         for j in range(len(file_list[i])):  # Iterate over elements of events (names or labels)
             name = ''.join(str(el) for el in file_list[i][j])
             name = name[2:len(name) - 2]
-            path = './input/WIDER_' + str(dataset) + '/images/' + str(event) + '/' + str(name) + '.jpg'
-            # image = cv2.imread(path)
+            jpg_dir = 'input/WIDER_' + str(dataset) + '/' + '/images/'
+            hdf5_dir = 'input/hdf5_' + str(dataset) + '/'
+            event_dir = str(event) + '/'
+            jpg_path = jpg_dir  + event_dir + str(name) + '.jpg'
+            h5_path = hdf5_dir + event_dir + str(name) + '.h5'
+            os.mkdir(hdf5_dir) if not os.path.isdir(hdf5_dir) else None
+            os.mkdir(hdf5_dir+event_dir) if not os.path.isdir(hdf5_dir+event_dir) else None
+            image = cv2.imread(jpg_path)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            shape = image.shape
+            hdf5_store(image, h5_path)
             box = [np.array(bbx) for bbx in face_bbx_list[i][j]]
             box = list(box[0])
             label_dict = [{'box': bbx, 'confidence': 1.0} for bbx in box]
-
             n_faces = len(label_dict)
-            data_dict.append({'id': count, 'event': event, 'path': path, 'n_faces': n_faces, 'labels': label_dict})
+            data_dict.append({'id': count, 'event': event, 'h5_path': h5_path, 'shape': shape, 'n_faces': n_faces, 'labels': label_dict })
             count += 1
 
-    if save:
-        file = open('input/' + str(dataset) + '_dict.pickle', 'wb')
-        pickle.dump(data_dict, file)
+    pickle_store(data_dict, 'input/' + dataset + '_dict.pickle')
 
     return data_dict
 
+# ======================================================================================================================
+#                                                   PLOTS
+# ======================================================================================================================
 
 def plot_box(image, labels, color='r', debug=False, show=True):
     fig, ax = plt.subplots()
@@ -73,32 +107,48 @@ def plot_box(image, labels, color='r', debug=False, show=True):
 def plot_box_from_dict(data_dict, event='all', max_iter=None):
     for i, d in enumerate(data_dict):
         if (d['event'] == str(event)) or (str(event) == 'all'):
-            image = Image.open(d['path'])
-            print("Path:", d['path'], "\t\tSize:", image.size)
+            image = hdf5_load(d['h5_path'])
+            print("Path:", d['h5_path'], "\t\tDimensions:", image.shape)
             plot_box(image, d['labels'])
             if i == max_iter - 1:
                 break
 
+# ======================================================================================================================
+#                                               PREPROCESSING
+# ======================================================================================================================
 
-def filter_small_bbx(data_dict, box_size, debug=False):
+def filter_box(data_dict, box_size, debug=False):
     print("Removing boxes with size < {}".format(box_size))
     clean = 0
     for d in data_dict:
-        print("Path: ", d['path']) if debug else None
+        print("Path: ", d['h5_path']) if debug else None
         print("Before: ", d['n_faces']) if debug else None
         d['labels'] = [label for label in d['labels'] if (label['box'][2]*label['box'][3]) > box_size]
         d['n_faces'] = len(d['labels'])
         print("After: ", d['n_faces']) if debug else None
         if d['n_faces'] == 0:
             clean = 1
-
     if clean:
         print("Found images with no labels. Removing them...")
         init_len = len(data_dict)
         data_dict = [d for d in data_dict if d['n_faces'] != 0]
         print("Removed images:", init_len-len(data_dict))
-
     return data_dict
+
+
+def heights_description(data_dict, ):
+    heights = []
+    for img in data_dict:
+        # h changes, while w and c are the same (w=1024, c=3)
+        heights.append(img['shape'][0])
+    print("\nHeight description:")
+    print(pd.Series(heights).describe(percentiles=[.05, .25, .5, .75, .80, .85, .90, .95]))  # Series is the type!
+    return heights
+
+
+
+
+
 
 
 
